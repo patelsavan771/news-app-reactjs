e41935ebb9a94bce50d0b82b9bdad29b4a15138a
Savan <patelsavan771@gmail.com>
2022-07-12 15:01:19 +0530

    Infinite scroll added all bugs solved

--- src/components/News.js
+++ src/components/News.js
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import NewsItem from "./NewsItem";
 import Spinner from "./Spinner";
 import PropTypes from 'prop-types';
+import InfiniteScroll from 'react-infinite-scroll-component';
+
 
 
 export class News extends Component {
@@ -37,16 +39,16 @@ export class News extends Component {
   displayNews = async () => {
       const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${this.props.apiKey}&pageSize=${this.props.pageSize}&page=${this.state.page}`;
 
-      this.setState({
-          loading: true,
-      });
+      // this.setState({
+      //     loading: true,
+      // });
       let data = await fetch(url);
       let parsedData = await data.json();
       console.log(parsedData);
       this.setState({
           articles: parsedData.articles,
           totalResults: parsedData.totalResults,
-          loading: false,
+          // loading: false,
       });
   }
 
@@ -68,6 +70,22 @@ export class News extends Component {
       // this.displayNews();  problem in which displayNews execute before state was updated.
   };
 
+  fetchMoreData = async () => {
+    this.setState({
+      page: this.state.page + 1
+    }, async () => {
+      const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${this.props.apiKey}&pageSize=${this.props.pageSize}&page=${this.state.page}`;
+
+      let data = await fetch(url);
+      let parsedData = await data.json();
+      console.log(parsedData);
+      this.setState({
+        articles: this.state.articles.concat(parsedData.articles),
+        totalResults: parsedData.totalResults,
+      });
+    });
+  }
+
 
   render() {
     return (
@@ -75,24 +93,35 @@ export class News extends Component {
         <div className="container my-3">
           <h2 className="my-3">{this.capitalFirstLetter(this.props.category)} - Top headlines</h2>
           {this.state.loading && <Spinner />}
-          <div className="row">
-              {!this.state.loading &&
-                  this.state.articles.map((ele) => {
-                      return (
-                          <div className="col-md-4" key={ele.url}>
-                              <NewsItem
-                                  title={ele.title}
-                                  description={ele.description}
-                                  imgUrl={ele.urlToImage}
-                                  newsUrl={ele.url}
-                                  author={ele.author}
-                                  date={ele.publishedAt}
-                              />
-                          </div>
-                      );
-                  })}
+
+          <InfiniteScroll
+            dataLength={this.state.articles.length}
+            next={this.fetchMoreData}
+            hasMore={this.state.articles.length !== this.state.totalResults}
+            loader={<Spinner />}
+          >
+          <div className="container">
+
+            <div className="row">
+                {this.state.articles.map((ele) => {
+                  return (
+                    <div className="col-md-4" key={ele.url}>
+                                <NewsItem
+                                    title={ele.title}
+                                    description={ele.description}
+                                    imgUrl={ele.urlToImage}
+                                    newsUrl={ele.url}
+                                    author={ele.author}
+                                    date={ele.publishedAt}
+                                    />
+                            </div>
+                        );
+                      })}
+            </div>
           </div>
-          <div className="container d-flex justify-content-between my-3">
+
+          </InfiniteScroll>
+          {/* <div className="container d-flex justify-content-between my-3">
               <button
                 disabled={this.state.page <= 1}
                 type="button"
@@ -112,7 +141,7 @@ export class News extends Component {
               >
                 Next &rarr;
               </button>
-          </div>
+          </div> */}
         </div>
       </>
     )
